9647831252e93f22b486d284078eef5aa52aea26
BabyMeat <157220534+BabyMeat@users.noreply.github.com>
2024-04-04 20:58:23 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+// Sélectionne l'élément contenant le texte à faire clignoter
+const textElement = document.querySelector('p');
+
+// Fonction pour faire clignoter le texte
+function blinkText() {
+    // Si la classe 'blink' est déjà présente, la retire, sinon l'ajoute
+    textElement.classList.toggle('blink');
+}
+
+// Appelle la fonction blinkText toutes les 500 millisecondes (0.5 secondes)
+setInterval(blinkText, 500);
